e36d959097b6507888efb28e4413df77b55919bd
Nathan <39814328+Yofou@users.noreply.github.com>
2019-06-24 22:56:27 +0100

    Added Tempory Channel fetures and Dynamic Help command
    Adds Dynmaic Help command where the general purpose is that it autolmatically adds the command in the help list without having the change the code of help command file.
    
    Adds Tempory channel feutures where the general purpose is to create tempory voice and text channel for the user so they can have a private group togeather on all servers.

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,33 @@
+const fs = require('fs');
+const discord = require('discord.js');
+
+// Dynamic Help command
+
 module.exports.run = async (client, message, args, prefix) => {
-    message.channel.sendMessage('test!')
-    .then().catch(console.error);
+    // Setup embed
+    const helpEmbed = new discord.RichEmbed()
+    .setTitle("Command Help list")
+    .setThumbnail("https://images.emojiterra.com/twitter/v12/512px/1f5a5.png")
+    .setColor("#00FFFF")
+
+    // Gather all command names and description
+    let commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+    // Iterate through commandfiles for every file
+    for (const file of commandFiles) {
+      const command = require(__dirname.replace("commands",`commands\\${file}`)); // Get info about each file
+
+      // Pushes the Feild name (command name) Description of the command into embed
+      if (command.help.dev == false || message.member.hasPermission('ADMINISTRATOR')){helpEmbed.addField(command.help.name,`	\`\`\`${command.help.description}\`\`\` `,false)}
+
+    }
+
+    // Send callback
+    message.reply( helpEmbed ).then().catch(console.error)
 }
 
 module.exports.help = {
-    name:"help",
-    description:"displays all commands",
+    name: "help",
+    description: "displays all commands",
     dev: false
 }
