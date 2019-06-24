5e45eee79107f40d8565429461fa09ebe4c18977
Nathan <39814328+Yofou@users.noreply.github.com>
2019-06-24 23:15:54 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,17 +10,23 @@ module.exports.run = async (client, message, args, prefix) => {
     .setThumbnail("https://images.emojiterra.com/twitter/v12/512px/1f5a5.png")
     .setColor("#00FFFF")
 
-    // Gather all command names and description
-    let commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-
     // Iterate through commandfiles for every file
-    for (const file of commandFiles) {
-      const command = require(__dirname.replace("commands",`commands\\${file}`)); // Get info about each file
+    // for (const file of commandFiles) {
+    //   const command = require(__dirname.replace("commands",`commands\\${file}`)); // Get info about each file
+    //
+    //   // Pushes the Feild name (command name) Description of the command into embed
+    //   if (command.help.dev == false || message.member.hasPermission('ADMINISTRATOR')){helpEmbed.addField(command.help.name,`	\`\`\`${command.help.description}\`\`\` `,false)}
+    //
+    // }
+
+    client.commands.forEach(element => {
+      let name = element.help.name;
+      let desc = element.help.description;
+      let dev = element.help.dev;
 
       // Pushes the Feild name (command name) Description of the command into embed
-      if (command.help.dev == false || message.member.hasPermission('ADMINISTRATOR')){helpEmbed.addField(command.help.name,`	\`\`\`${command.help.description}\`\`\` `,false)}
-
-    }
+      if (dev == false || message.member.hasPermission('ADMINISTRATOR')){helpEmbed.addField(name,`	\`\`\`${desc}\`\`\` `,false)}
+    });
 
     // Send callback
     message.reply( helpEmbed ).then().catch(console.error)
