cc80cf200586054c2e325a1637a1f6903169f587
rumblefrog <contact@zhenyangli.me>
2019-06-28 03:58:14 -0400

    patch logger

--- src/log.js
+++ src/log.js
@@ -2,13 +2,14 @@ import { createLogger, format, transports } from 'winston';
 const { label, combine, timestamp, printf, colorize } = format;
 
 const log = createLogger({
+    level: 'debug',
     format: combine(
         colorize({
-            error: 'red',
-            warn: 'yellow',
-            info: 'green',
-            verbose: 'white',
             debug: 'blue',
+            verbose: 'white',
+            info: 'green',
+            warn: 'yellow',
+            error: 'red',
         }),
         label({
             label: 'Grouper',
@@ -19,11 +20,11 @@ const log = createLogger({
         })
     ),
     levels: {
-        debug: 0,
-        verbose: 1,
+        debug: 4,
+        verbose: 3,
         info: 2,
-        warn: 3,
-        error: 4,
+        warn: 1,
+        error: 0,
     },
     transports: [
         new transports.Console()
