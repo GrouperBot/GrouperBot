9c5630f9f20a58e1a73e8455c6fd46324512c33c
rumblefrog <contact@zhenyangli.me>
2019-06-27 23:52:43 -0400

    schema: increase tag width

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -3,14 +3,14 @@
  * 
  * @type {string}
  */
-const CreateTagTable = "CREATE TABLE IF NOT EXISTS `tags` ( `name` VARCHAR(8) NOT NULL , `created_at` BIGINT NOT NULL , PRIMARY KEY (`name`)) ENGINE = InnoDB;";
+const CreateTagTable = "CREATE TABLE IF NOT EXISTS `tags` ( `name` VARCHAR(16) NOT NULL , `created_at` BIGINT NOT NULL , PRIMARY KEY (`name`)) ENGINE = InnoDB;";
 
 /**
  * Advertisement table creation SQL query
  * 
  * @type {string}
  */
-const CreateAdvertisementTable = "CREATE TABLE IF NOT EXISTS `advertisements` ( `id` INT NOT NULL AUTO_INCREMENT , `tags` VARCHAR(64) NOT NULL , `players` INT NOT NULL , `description` TEXT NOT NULL , `expiration` BIGINT NOT NULL , `created_at` BIGINT NOT NULL , PRIMARY KEY (`id`), INDEX (`tags`), INDEX (`expiration`)) ENGINE = InnoDB;";
+const CreateAdvertisementTable = "CREATE TABLE IF NOT EXISTS `advertisements` ( `id` INT NOT NULL AUTO_INCREMENT , `tags` VARCHAR(128) NOT NULL , `players` INT NOT NULL , `description` TEXT NOT NULL , `expiration` BIGINT NOT NULL , `created_at` BIGINT NOT NULL , PRIMARY KEY (`id`), INDEX (`tags`), INDEX (`expiration`)) ENGINE = InnoDB;";
 
 /**
  * Regex for parsing arguments
